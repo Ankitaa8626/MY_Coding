bd60b06a0d4a0d84411a3c68c20e4ca5b4897682
Ankitaa8626 <ankandoi@gmail.com>
2015-10-05 16:35:06 +0530

    private is not visible outside a class

---
+++ Newrepoo/src/Newrepo/Capsi.java
@@ -0,0 +1,30 @@
+package Newrepo;
+
+public class Capsi {
+	int a;
+	private int b;
+	protected int c;
+	public int d;
+
+	protected int division(int c, int d) {
+		int value = c / d;
+		return value;
+	}
+
+	int sum(int a, int b) {
+		int result = a + b;
+		return result;
+
+	}
+
+	public int multiply(int b, int c) {
+		int output = b * c;
+		return output;
+	}
+
+	private int sub(int c, int a) {
+		int out = c - a;
+		return out;
+	}
+
+}
