8ea2d4abded58f7e4c41fe38bd8ff74d4a9ab252
Ankitaa8626 <ankandoi@gmail.com>
2015-10-10 10:14:51 +0530

    inheritance practice

---
+++ Inheritance/src/com/clc/learning/sub/ChildClass.java
@@ -0,0 +1,20 @@
+package com.clc.learning.sub;
+
+import com.clc.learning.superclass.Parent;
+
+public class ChildClass extends Parent {
+	public int weight=45;
+	public int height=5;
+	public void son(){
+		System.out.println("child class");
+		
+		
+	}
+	public static void main(String[] args) {
+		ChildClass a=new ChildClass();
+		Parent b=new Parent();
+		System.out.println(a.age);
+		//System.out.println(b.age);
+	}
+
+}
---
+++ Inheritance/src/com/clc/learning/superclass/Parent.java
@@ -0,0 +1,11 @@
+package com.clc.learning.superclass;
+
+public class Parent {
+	public int weight=40;
+	protected int age=22;
+	public void father(){
+		System.out.println("parent class");
+		
+	}
+
+}
